79367b65d02cd623eff327590e0015ed7a42957b
mark <markpackham1@gmail.com>
2022-06-29 15:25:40 +0100

    demo use of layout
    https://youtu.be/ydR_M0fw9Xc?t=3172

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import global from '../global.css';
+</script>
+
+<svelte:head>
+	<title>MovieDB Svelte</title>
+</svelte:head>
+
+<nav>
+	<ul>
+		<li>I am the layout and I show up everywhere</li>
+	</ul>
+</nav>
+<slot />
