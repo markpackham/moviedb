fa2b50d22944125a33b64254a5e7d0e312ae3d06
mark <markpackham1@gmail.com>
2022-06-29 14:50:08 +0100

    show info on specific movie
    https://youtu.be/ydR_M0fw9Xc?t=2786

--- src/routes/movie/[id].svelte
+++ src/routes/movie/[id].svelte
@@ -1,1 +1,30 @@
-<h1>Hello</h1>
+<script context="module">
+	export async function load({ fetch, params }) {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/movie/${params.id}?api_key=d5c35e51c81488b19da7c1f572507a3d&language=en-US`
+		);
+
+		const movieDetail = await res.json();
+		if (res.ok) {
+			return {
+				props: { movieDetail }
+			};
+		}
+	}
+</script>
+
+<script>
+	export let movieDetail;
+</script>
+
+<div class="movie-details">
+	<div class="img-container">
+		<img
+			src={'https://image.tmdb.org/t/p/original' + movieDetail.backdrop_path}
+			alt={movieDetail.title}
+		/>
+	</div>
+	<div class="text-container">
+		<h1>{movieDetail.title}</h1>
+	</div>
+</div>
