a380bde962f158cabf7a5fbb070b68e75997169a
mark <markpackham1@gmail.com>
2022-06-29 15:29:56 +0100

    add nav
    https://youtu.be/ydR_M0fw9Xc?t=3245

---
+++ src/components/Nav.svelte
@@ -0,0 +1,17 @@
+<nav>
+	<a href="/">Movie Database Home</a>
+</nav>
+
+<style>
+	nav {
+		display: flex;
+		min-height: 10vh;
+		align-items: center;
+		justify-content: center;
+	}
+	a {
+		font-size: 1rem;
+		font-weight: bold;
+		color: black;
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,10 @@
 <script>
 	import global from '../global.css';
+	import Nav from '../components/Nav.svelte';
 </script>
 
 <svelte:head>
 	<title>MovieDB Svelte</title>
 </svelte:head>
-
-<nav>
-	<ul>
-		<li>I am the layout and I show up everywhere</li>
-	</ul>
-</nav>
+<Nav />
 <slot />
