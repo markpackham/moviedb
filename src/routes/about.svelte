c0894616143e89ec5e0bafa8ca88ee8544af3aab
mark <markpackham1@gmail.com>
2022-06-29 13:17:13 +0100

    export data from api
    https://youtu.be/ydR_M0fw9Xc?t=1104

--- src/routes/about.svelte
+++
@@ -1,5 +0,0 @@
-<p>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat impedit ducimus dignissimos
-	natus. Facere commodi aperiam quis? In, quos laborum rerum vel deleniti reprehenderit ab iusto
-	totam distinctio possimus dolorum!
-</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,19 @@
-<script>
-	let count = 0;
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/movie/popular?api_key=d5c35e51c81488b19da7c1f572507a3d&language=en-US&page=1`
+		);
 
-	function incrementBtn() {
-		count++;
+		const data = await res.json();
+		if (res.ok) {
+			return {
+				props: { popular: data.results }
+			};
+		}
 	}
 </script>
 
-<h1>Number: {count}</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/about">Go to about</a>
-
-<button on:click={incrementBtn}>Increment</button>
+<script>
+	export let popular;
+	console.log(popular);
+</script>
