f43b5b4f268c5c90ee4f40f55860959e3f6fbf9e
mark <markpackham1@gmail.com>
2022-06-29 12:59:08 +0100

    Revise Svelte from https://youtu.be/ydR_M0fw9Xc?t=646

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	let count = 0;
+
+	function incrementBtn() {
+		count++;
+	}
+</script>
+
+<h1>Number: {count}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="/about">Go to about</a>
+
+<button on:click={incrementBtn}>Increment</button>
