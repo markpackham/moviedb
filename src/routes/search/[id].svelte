f8412dd216a1868906e8317cd13218df8bf2dcd6
mark <markpackham1@gmail.com>
2022-06-30 11:35:41 +0100

    finish search
    https://youtu.be/ydR_M0fw9Xc?t=4450

--- src/routes/search/[id].svelte
+++ src/routes/search/[id].svelte
@@ -12,3 +12,24 @@
 		}
 	}
 </script>
+
+<script>
+	import MovieCard from '../../components/MovieCard.svelte';
+	export let searchedMovie;
+</script>
+
+<div class="searched-movies">
+	{#each searchedMovie as movie}
+		<MovieCard {movie} />
+	{/each}
+</div>
+
+<style>
+	.searched-movies {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+		grid-column-gap: 1rem;
+		grid-row-gap: 2rem;
+		height: 20vh;
+	}
+</style>
