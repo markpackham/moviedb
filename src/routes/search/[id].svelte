2c4c315fa932b059176fac46d0a07c4be48a37f2
mark <markpackham1@gmail.com>
2022-06-30 10:59:12 +0100

    add search id page
    https://youtu.be/ydR_M0fw9Xc?t=4134

---
+++ src/routes/search/[id].svelte
@@ -0,0 +1,14 @@
+<script context="module">
+	export async function load({ fetch, params }) {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/search/movie?api_key=d5c35e51c81488b19da7c1f572507a3d&language=en-US&query=${params.id}&page=1&include_adult=false`
+		);
+
+		const data = await res.json();
+		if (res.ok) {
+			return {
+				props: { searchedMovie: data.results }
+			};
+		}
+	}
+</script>
